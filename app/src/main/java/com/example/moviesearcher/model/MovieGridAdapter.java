c6bdcece039f2e43d467a649733dd1155788849f
Jokubas Serapinas <43211120+Jokubas126@users.noreply.github.com>
2020-01-25 13:42:35 +0100

    gridView done to start working with it, main parts of the layout of movie details done

--- app/src/main/java/com/example/moviesearcher/model/MovieGridAdapter.java
+++ app/src/main/java/com/example/moviesearcher/model/MovieGridAdapter.java
@@ -28,7 +28,7 @@ public class MovieGridAdapter extends RecyclerView.Adapter<MovieGridAdapter.View
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_movie_grid, parent, false);
+                .inflate(R.layout.item_movie, parent, false);
         return new ViewHolder(view);
     }
 
