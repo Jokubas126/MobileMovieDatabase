dab68e03009b8d306680a8ba2e35923af4f87031
Jokubas <jokubas@shape.dk>
2020-12-26 23:50:54 +0200

    added and initially set roboto and roboto condensed fonts

--- util/src/main/java/com/jokubas/mmdb/util/LayoutUtil.kt
+++ util/src/main/java/com/jokubas/mmdb/util/LayoutUtil.kt
@@ -5,6 +5,7 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
+//TODO set something up for restoring the state
 @BindingAdapter("movieGridLayoutManager")
 fun RecyclerView.setMovieGridLayoutManager(landscapeSpanCount: Int = 4) {
     layoutManager = when (context.resources.configuration.orientation) {
@@ -15,15 +16,3 @@ fun RecyclerView.setMovieGridLayoutManager(landscapeSpanCount: Int = 4) {
         else -> StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
     }
 }
-
-/*
-@BindingAdapter(value = ["isDataLoading", "addData"], requireAll = true)
-fun RecyclerView.loadDataOnScrolledDown(isDataLoading: Boolean, addData: () -> Unit) {
-    addOnScrollListener(object : RecyclerView.OnScrollListener() {
-        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-            super.onScrollStateChanged(recyclerView, newState)
-            if (!canScrollVertically(1) && !isDataLoading)
-                addData.invoke()
-        }
-    })
-}*/
